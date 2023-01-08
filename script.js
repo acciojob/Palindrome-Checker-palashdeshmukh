24fe71608b9f7723feba4bc98b478f5950861667
Palash Deshmukh <115765112+palashdeshmukh@users.noreply.github.com>
2023-01-08 08:35:49 -0500

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+	let str2 = "";
+	let len2 = str.length();
+	
 
+		for (let i =len2 -1, i >= 0; i-- ){
+				str2.append(str[i])
+		}
+		if (str2 == str){
+			return true;
+		}
+		else{
+			return false;
+		}
 }
 module.exports = palindrome
